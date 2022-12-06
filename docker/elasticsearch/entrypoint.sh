eb98c222fa1542d32038fb09716bff9ad526e30e
oec <oec@dogrun.jp>
2022-12-06 19:36:59 +0900

    Configuration without data

--- docker/elasticsearch/entrypoint.sh
+++ docker/elasticsearch/entrypoint.sh
@@ -14,15 +14,37 @@ done
 
 echo "Start to input initial data."
 
+# BioProjectのみマッピングを試す
+# BIOPROJECTのマッピング
+curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/bioproject_mapping -d @/usr/share/elasticsearch/config/templates/bioproject-mapping.json > /dev/null 2>&1
+
+echo "Finish to input BioProject"
+
+
+:<<'#COMMENT_OUT'
+# BIOSAMPLEのマッピング
+curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/biosample_mapping -d @/usr/share/elasticsearch/config/templates/biosample-mapping.json > /dev/null 2>&1
+
+echo "Finish to input BioSample"
+
+
+# DRAのマッピング
+curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/sra_submission_mapping -d @/usr/share/elasticsearch/config/templates/sra-submission-mapping.json > /dev/null 2>&1
+
+echo "Finish to input Submission"
+
+#COMMENT_OUT
+
+:<<'#COMMENT_OUT'
 # JGAのマッピング
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/jga_study_mapping -d @/usr/share/elasticsearch/config/templates/jga-study-mapping.json > /dev/null 2>&1
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/jga_dataset_mapping -d @/usr/share/elasticsearch/config/templates/jga-dataset-mapping.json > /dev/null 2>&1
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/jga_policy_mapping -d @/usr/share/elasticsearch/config/templates/jga-policy-mapping.json > /dev/null 2>&1
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/jga_dac_mapping -d @/usr/share/elasticsearch/config/templates/jga-dac-mapping.json > /dev/null 2>&1
+#COMMENT_OUT
 
-# BIOPROJECTのマッピング
-curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/bioproject_mapping -d @/usr/share/elasticsearch/config/templates/bioproject-mapping.json > /dev/null 2>&1
 
+:<<'#COMMENT_OUT'
 # BIOSAMPLEのマッピング
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/biosample_mapping -d @/usr/share/elasticsearch/config/templates/biosample-mapping.json > /dev/null 2>&1
 
@@ -33,6 +55,9 @@ curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/sra
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/sra_run_mapping -d @/usr/share/elasticsearch/config/templates/sra-run-mapping.json > /dev/null 2>&1
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/sra_study_mapping -d @/usr/share/elasticsearch/config/templates/sra-study-mapping.json > /dev/null 2>&1
 curl -H "Content-Type:application/json" -XPUT localhost:9200/_index_template/sra_sample_mapping -d @/usr/share/elasticsearch/config/templates/sra-sample-mapping.json > /dev/null 2>&1
+#COMMENT_OUT
 
 echo "Finish to input initial data."
 tail -f /dev/null
+                                                                                                                                                                               
+                     
